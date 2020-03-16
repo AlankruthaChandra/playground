69f1694eec340ad451e1f8a87d210ac4394bc256
kiran <reddysai616@gmail.com>
2020-03-16 23:00:23 +0530

    added student folder

---
+++ pages/student/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default () => (
+  <div className="hero">
+    <h1 className="title">Student dashboard</h1>{" "}
+    <p className="font-bold">student info.. </p>
+    <p className="font-semibold">student attandance.. </p>
+    <p className="font-bold">student marks.. </p>
+    <p className="font-bold text-red-700">student .. </p>
+
+  </div>
+);
